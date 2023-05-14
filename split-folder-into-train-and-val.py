918e4e50a24537a469eed73ff609d4a4a4f77a9a
ngoquocthai0311 <ngoquocthai0311@gmail.com>
2023-05-14 18:35:18 +0700

    refactor split folder into train and validate folder script

--- split-folder-into-train-and-val.py
+++ split-folder-into-train-and-val.py
@@ -17,14 +17,15 @@ full_path_MIAS = os.path.join(absolute_path, relative_path_MIAS)
 full_path_combineddataset = os.path.join(absolute_path, relative_path_combineddataset)
 
 
-splitfolders.ratio(full_path_DDSM, output=os.path.join(OUTPUT_FOLFDER, relative_path_DDSM),
-    seed=1337, ratio=(.8, .2), group_prefix=None, move=False) # default values
+def split_preprocessed_images_to_train_and_val_folder(train_ratio = .8, validate_ratio = .2):
+    splitfolders.ratio(full_path_DDSM, output=os.path.join(OUTPUT_FOLFDER, relative_path_DDSM),
+    seed=1337, ratio=(train_ratio, validate_ratio), group_prefix=None, move=False) # default values
 
-splitfolders.ratio(full_path_INbreast, output=os.path.join(OUTPUT_FOLFDER, relative_path_Inbreast),
-    seed=1337, ratio=(.8, .2), group_prefix=None, move=False) # default values
+    splitfolders.ratio(full_path_INbreast, output=os.path.join(OUTPUT_FOLFDER, relative_path_Inbreast),
+        seed=1337, ratio=(train_ratio, validate_ratio), group_prefix=None, move=False) # default values
 
-splitfolders.ratio(full_path_MIAS, output=os.path.join(OUTPUT_FOLFDER, relative_path_MIAS),
-    seed=1337, ratio=(.8, .2), group_prefix=None, move=False) # default values
+    splitfolders.ratio(full_path_MIAS, output=os.path.join(OUTPUT_FOLFDER, relative_path_MIAS),
+        seed=1337, ratio=(train_ratio, validate_ratio), group_prefix=None, move=False) # default values
 
-splitfolders.ratio(full_path_combineddataset, output=os.path.join(OUTPUT_FOLFDER, relative_path_combineddataset),
-    seed=1337, ratio=(.8, .2), group_prefix=None, move=False) # default values
+    splitfolders.ratio(full_path_combineddataset, output=os.path.join(OUTPUT_FOLFDER, relative_path_combineddataset),
+        seed=1337, ratio=(train_ratio, validate_ratio), group_prefix=None, move=False) # default values
